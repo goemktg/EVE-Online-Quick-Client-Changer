57a2028cc7e9b45cc7a04dbda2ec6a3785649d7c
goemktg <samktg52@gmail.com>
2024-11-28 06:04:32 +0900

    WPF로 재작성 ( 기본 틀 작성 )

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EVE_Online_Quick_Client_Changer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
