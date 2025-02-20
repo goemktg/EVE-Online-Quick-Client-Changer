b31e063f809c82e764ccbe45bdeec22550cac873
goemktg <samktg52@gmail.com>
2025-02-21 08:00:14 +0900

    feat: 이브 클라이언트 불러오기 기능 추가

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,48 @@ namespace EVE_Online_Quick_Client_Changer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<EveClientData> EveClients = [];
+
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadListBoxData();
         }
+
+        private void LoadListBoxData()
+        {
+            // Search for all EVE Online clients
+            System.Diagnostics.Process[] eveProcesses = System.Diagnostics.Process.GetProcessesByName("exefile");
+
+            string hotKeyNamePlaceholder = "없음";
+            int hotKeyIDPlaceholder = -1;
+
+            foreach (System.Diagnostics.Process process in eveProcesses)
+            {
+                // skip EVE clients in login screen
+                if (process.MainWindowTitle == "EVE")
+                    continue;
+
+                EveClients.Add(new EveClientData()
+                {
+                    ProcessID = process.Id,
+                    MainWindowTitle = process.MainWindowTitle,
+                    HotKeyName = hotKeyNamePlaceholder,
+                    HotKeyID = hotKeyIDPlaceholder
+                });
+            }
+
+            // Load data to listbox
+            lbEveClients.ItemsSource = EveClients;
+        }
+    }
+
+    public class EveClientData
+    {
+        public int ProcessID { get; set; }
+        public required string MainWindowTitle { get; set; }
+        public required string HotKeyName { get; set; }
+        public int HotKeyID { get; set; }
     }
 }
